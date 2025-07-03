045ecf986066fe88e7b348ec53402507ac7533cc
anas-fareedi <anasfareedi786786@gmail.com>
2025-07-03 12:47:51 +0530

    add db url

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,9 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+SQLALCHEMY_DATABASE_URL="postgresql://tododb_dimv_user:FPvKnZIKBQ59HbfJANb8cKz2zNxHY4Ci@dpg-d1j2p5h5pdvs73cnqsd0-a.oregon-postgres.render.com/tododb_dimv"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
